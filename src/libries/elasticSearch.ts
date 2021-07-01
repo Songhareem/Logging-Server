d9a88cb7ea38d078ad339cb39035a5ebdedb57a8
Songhareem <elaha00@naver.com>
2021-07-01 15:46:50 +0900

    update

--- src/libries/elasticSearch.ts
+++ src/libries/elasticSearch.ts
@@ -3,7 +3,7 @@ import { Client, RequestParams } from '@elastic/elasticsearch';
 
 // Elasticsearch를 사용하기 위해 객체를 생성합니다.
 const client = new Client({
-    node: "http://localhost:9200"
+    node: "http://hygit.iptime.org:8000"
 });
 
 export abstract class ElasticSearch<T> {
@@ -17,6 +17,7 @@ export abstract class ElasticSearch<T> {
     protected async requestElasticSearch(bodyData: RequestParams.Index) {
         
         // Elasticsearch 객체에서는 index 라는 메소드를 사용해 elasticsearch로 데이터를 보낼 수 있습니다.
+        console.log(bodyData);
         await client.index(bodyData);
     }
 
